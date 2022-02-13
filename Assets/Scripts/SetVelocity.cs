982d8ab0f451a092bc4ac5b7b832b93889b06dd4
Bruno Lessa <brunolessa@live.com>
2022-02-13 18:13:06 -0300

    up

--- Assets/Scripts/SetVelocity.cs
+++ Assets/Scripts/SetVelocity.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class SetVelocity : MonoBehaviour
 {
     [SerializeField] private Vector3 velocity;
+    [SerializeField] private Vector3 angularVelocity;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             var rb = GetComponent<Rigidbody>();
+            rb.angularVelocity = angularVelocity;
+            rb.maxAngularVelocity = float.MaxValue;
             rb.velocity = velocity;
             enabled = false;
         }
