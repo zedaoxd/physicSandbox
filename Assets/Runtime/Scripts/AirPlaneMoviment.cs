2c8ec7cd041cff7c0848d8606991fd8ae86cb68c
Bruno Lessa <brunolessa@live.com>
2022-02-14 16:54:49 -0300

    finish controller

--- Assets/Runtime/Scripts/AirPlaneMoviment.cs
+++ Assets/Runtime/Scripts/AirPlaneMoviment.cs
@@ -15,12 +15,17 @@ public class AirPlaneMoviment : MonoBehaviour
     [SerializeField] private float pitchTorque = 30;
     [SerializeField] private float yawTorque = 15;
     [SerializeField] private float thrust = 60;
-
+    [SerializeField] private float xRotClamp = 70;
+    
     [Header("Rigidbody")] 
     [SerializeField] private float mass = 10;
     [SerializeField] private float drag = 0.5f;
     [SerializeField] private float angularDrag = 10;
-    
+
+    [Header("Visuals")] 
+    [SerializeField] private Transform graphics;
+    [SerializeField] private float maxVisualRollAngle = 60;
+    [SerializeField] private float visualRollAcc = 2;
 
     private void Awake()
     {
@@ -41,16 +46,53 @@ public class AirPlaneMoviment : MonoBehaviour
         UpdateSteering();
     }
 
+    private static float To180Angle(float angle)
+    {
+        angle %= 360;
+        if (angle > 180) return angle - 360; // greater than 180
+        if (angle < -180) return angle + 360; // less than -180 
+        return angle; // between -180 and 180 
+    }
+
     private void UpdateSteering()
     {
+        // add Torque
         _steerInput = Vector2.Lerp(_steerInput, _frameInput, Time.fixedTime * steerSmoothSpeed);
         var torque = new Vector3(
             _steerInput.x * pitchTorque,
             _steerInput.y * yawTorque,
             0
         );
-
+        
+        if (ShouldBlockPitch())
+        {
+            _steerInput.x = 0;
+            torque.x = 0;
+        }
         _rb.AddRelativeTorque(torque);
+        
+        // Force no Z rotation
+        var correctedRot = _rb.rotation.eulerAngles;
+        correctedRot.z = 0;
+        _rb.rotation = Quaternion.Euler(correctedRot);
+        
+        // Graphics
+        var graphicsLocalRotation = graphics.localEulerAngles;
+        var targetZ = -maxVisualRollAngle * _steerInput.y;
+        graphicsLocalRotation.z = Mathf.MoveTowardsAngle(
+            graphicsLocalRotation.z, 
+            targetZ, 
+            Time.fixedDeltaTime * visualRollAcc
+        );
+        graphics.localEulerAngles = graphicsLocalRotation;
+    }
+
+    private bool ShouldBlockPitch()
+    {
+        var xRot = To180Angle(_rb.rotation.eulerAngles.x);
+        //Se ja passamos do limite de inclinação e ainda esta tentando mover naquela direção, bloqueia o movimento
+        return (_frameInput.x > 0 && xRot > xRotClamp) ||
+               (_frameInput.x < 0 && xRot < -xRotClamp);
     }
 
     private void UpdateMovement()
